1f1895ac0bcfdf61634a4d5ce275a06117765c43
zni4 <38941259+zni4@users.noreply.github.com>
2020-03-09 10:21:25 +0100

    Creacion del ejemplo

---
+++ svg_Animation/js/index.js
@@ -0,0 +1,10 @@
+function animar() {
+    if (document.getElementById("ipad").classList.contains("animar")) {
+        document.getElementById("ipad").classList.remove("animar");
+        document.getElementById("ipad").classList.add("des_animar");
+    }
+    else {
+        document.getElementById("ipad").classList.add("animar");
+        document.getElementById("ipad").classList.remove("des_animar");
+    }
+}
