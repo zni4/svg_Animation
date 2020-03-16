72afd9d3ef91313d336bed8dc7340cf1d326d2ce
zni4 <zni_4@hotmail.com>
2020-03-16 17:28:16 +0100

    Cambiamos svg para que no haya que hacer traslación

--- js/index.js
+++ js/index.js
@@ -1,10 +1,9 @@
 function animar() {
-    if (document.getElementById("ipad").classList.contains("animar")) {
-        document.getElementById("ipad").classList.remove("animar");
-        document.getElementById("ipad").classList.add("des_animar");
-    }
-    else {
-        document.getElementById("ipad").classList.add("animar");
-        document.getElementById("ipad").classList.remove("des_animar");
-    }
+	if (document.getElementById('ipad').classList.contains('animar')) {
+		document.getElementById('ipad').classList.remove('animar');
+		document.getElementById('ipad').classList.add('des_animar');
+	} else {
+		document.getElementById('ipad').classList.add('animar');
+		document.getElementById('ipad').classList.remove('des_animar');
+	}
 }
