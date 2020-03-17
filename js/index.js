b6236c387f7df9f2bad1ff61788a2be9c3636ffa
zni4 <zni_4@hotmail.com>
2020-03-17 16:08:55 +0100

    Animamos cada uno de los elementos

--- js/index.js
+++ js/index.js
@@ -2,8 +2,20 @@ function animar() {
 	if (document.getElementById('ipad').classList.contains('animar')) {
 		document.getElementById('ipad').classList.remove('animar');
 		document.getElementById('ipad').classList.add('des_animar');
+
+		document.getElementById('imagen_ipad').classList.add('desanimar_imagen');
+		document.getElementById('imagen_ipad').classList.remove('animar_imagen');
+
+		document.getElementById('textoiPad').classList.add('desanimar_texto');
+		document.getElementById('textoiPad').classList.remove('animar_texto');
 	} else {
 		document.getElementById('ipad').classList.add('animar');
 		document.getElementById('ipad').classList.remove('des_animar');
+
+		document.getElementById('imagen_ipad').classList.add('animar_imagen');
+		document.getElementById('imagen_ipad').classList.remove('desanimar_imagen');
+
+		document.getElementById('textoiPad').classList.add('animar_texto');
+		document.getElementById('textoiPad').classList.remove('desanimar_texto');
 	}
 }
